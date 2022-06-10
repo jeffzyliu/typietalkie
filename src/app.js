d7969c8272c31a6a453de71d155c794c2d516355
Jeff Liu <jeffzyliu@gmail.com>
2022-06-09 20:06:38 -0700

    feat: basic cardzilla scaffold

--- src/app.js
+++ src/app.js
@@ -1,7 +1,41 @@
-import React from 'react';
+/* eslint-disable react/jsx-props-no-spreading */
+import React, {
+  useEffect,
+  useState,
+  createRef,
+} from 'react';
+import { useSwipeable } from 'react-swipeable';
 
 function App() {
-  return <div>hi!</div>;
+  const textAreaRef = createRef();
+
+  const [height, setHeight] = useState(`${window.innerHeight}px`);
+  const [text, setText] = useState('');
+
+  useEffect(() => {
+    window.visualViewport.addEventListener(
+      'resize',
+      (event) => {
+        setHeight(`${event.target.height}px`);
+      },
+    );
+  }, []);
+
+  const handlers = useSwipeable({
+    onSwipedLeft: () => setText(''),
+    onSwipedDown: () => textAreaRef.current?.blur?.(),
+  });
+
+  return (
+    <div {...handlers}>
+      <textarea
+        style={{ height }}
+        value={text}
+        onChange={(event) => setText(event.target.value)}
+        ref={textAreaRef}
+      />
+    </div>
+  );
 }
 
 export default App;
