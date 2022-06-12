a886cd4175ac0e5411aa57838599ad5bdb1d2854
Jeff Liu <jeffzyliu@gmail.com>
2022-06-11 23:19:31 -0700

    fix: HMR

--- src/index.js
+++ src/index.js
@@ -6,8 +6,13 @@ import './style.scss';
 import App from './app';
 
 const root = createRoot(document.getElementById('main'));
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 );
+
+if (module.hot) {
+  module.hot.accept('./app');
+}
